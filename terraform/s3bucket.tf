d802a8770d0a0cdf61bcca3933440f96b5eb4bd4
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-05-25 20:13:26 +1000

    chore: update S3 bucket policy to restrict access to CloudFront only

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -31,12 +31,21 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Sid       = "PublicReadGetObject",
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.frontend.arn}/*"
-    }]
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontOACReadAccess",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.frontend.arn}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::864981714330:distribution/E1JJXRQXMWRYB4"
+          }
+        }
+      }
+    ]
   })
 }
