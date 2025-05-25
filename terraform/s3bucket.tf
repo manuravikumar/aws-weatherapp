5ef007ed559f1dbeeaf1185fadbd9e83190fa34c
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-05-25 20:51:51 +1000

    fix: update API endpoint in index.html and refine S3 bucket policy for CloudFront access

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -29,23 +29,31 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
 
   bucket = aws_s3_bucket.frontend.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontOACReadAccess",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        },
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.frontend.arn}/*",
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = "arn:aws:cloudfront::864981714330:distribution/E1JJXRQXMWRYB4"
-          }
+ policy = jsonencode({
+  Version = "2012-10-17",
+  Statement = [
+    {
+      Sid       = "AllowCloudFrontOACReadAccess",
+      Effect    = "Allow",
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      },
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.frontend.arn}/*",
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = "arn:aws:cloudfront::864981714330:distribution/E1JJXRQXMWRYB4"
         }
       }
-    ]
-  })
+    },
+    {
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }
+  ]
+})
+
 }
